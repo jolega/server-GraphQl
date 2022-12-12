aef07d28c89d11b235fc265de1f0495bc74785b5
jolega <alonso_juancho@hotmail.com>
2022-12-12 14:58:58 -0500

    primer peticion query

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import { gql } from 'apollo-server';
+import { ApolloServer,gql } from 'apollo-server';
 
 const  typeDefs = gql `
+
+  schema {
+    query: Query
+  }
    type Query {
      greeting: String
    }
 `;
 
-console.log(typeDefs)
+const resolvers = {
+  Query : {
+    greeting: () => 'Hello World'
+  }
+}
+
+const server = new ApolloServer({ typeDefs , resolvers }) ;
+const {url} = await server.listen({ port:9000});
+console.log(`Server Running at ${url}`)
